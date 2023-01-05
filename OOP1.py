d313e1bd5cd0b7e554407512b69b89f57c1f0fe3
Nitin <nitinsharma01001@gmail.com>
2023-01-05 14:25:54 +0530

    inheritance

--- OOP1.py
+++ OOP1.py
@@ -47,7 +47,7 @@ class Item:
 
 
     def __repr__(self):
-        return f"Item ('{self.name}', {self.price}, {self.quantity})"
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
 
 
 
@@ -55,5 +55,26 @@ class Item:
 # Item.instantiate_from_csv()
 # print(Item.all)    # here it is by reading through the files
 
-print(Item.is_integer(7))
+# print(Item.is_integer(7))
+class Phone(Item):       # for inheritance inheriting methods of item in phone
+    
+    def __init__(self, name: str, price: float, quantity=0, broken_phones = 0):
+        super().__init__(
+            name, price , quantity
+        )
+        assert price >= 0, f"Price {price} is not greater than or equal to zero, but lesss than zero."  # by assert we are providing the specifications or the conditions that should be applied to the method inputs
+
+        assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to zero, but lesss than zero."
+        assert broken_phones >= 0, f"Broken phones {broken_phones} is not greater than or equal to zero, but lesss than zero."
+
+        self.name = name  # These are the method attributes
+        self.price = price
+        self.quantity = quantity
+        self.broken_phones = broken_phones
+
+phone1 = Phone("jscPhonev10", 500, 5, 1)
+print(Item.all)
+print(Phone.all)
+
+
 
